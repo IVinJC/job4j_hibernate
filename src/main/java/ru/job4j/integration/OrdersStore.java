c9c8b5dbb70d6ef0d5422848c610a8be170c0aa7
Windows <ivinjc@gmail.com>
2022-08-07 17:00:57 +0300

    Что такое интеграционное тестирование. [#6875]

---
+++ src/main/java/ru/job4j/integration/OrdersStore.java
@@ -0,0 +1,116 @@
+package ru.job4j.integration;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class OrdersStore {
+    private final BasicDataSource pool;
+
+    public OrdersStore(BasicDataSource pool) {
+        this.pool = pool;
+    }
+
+    public Order save(Order order) {
+        try (Connection con = pool.getConnection();
+             PreparedStatement pr = con.prepareStatement(
+                     "INSERT INTO orders(name, description, created) VALUES (?, ?, ?)",
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+            pr.setString(1, order.getName());
+            pr.setString(2, order.getDescription());
+            pr.setTimestamp(3, order.getCreated());
+            pr.execute();
+            ResultSet id = pr.getGeneratedKeys();
+            if (id.next()) {
+                order.setId(id.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return order;
+    }
+
+    public Collection<Order> findAll() {
+        List<Order> rsl = new ArrayList<>();
+        try (Connection con = pool.getConnection();
+             PreparedStatement pr = con.prepareStatement("SELECT * FROM orders")) {
+            try (ResultSet rs = pr.executeQuery()) {
+                while (rs.next()) {
+                    rsl.add(
+                            new Order(
+                                    rs.getInt("id"),
+                                    rs.getString("name"),
+                                    rs.getString("description"),
+                                    rs.getTimestamp(4)
+                            )
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public Order findById(int id) {
+        Order rsl = null;
+        try (Connection con = pool.getConnection();
+             PreparedStatement pr = con.prepareStatement("SELECT * FROM orders WHERE id = ?")) {
+            pr.setInt(1, id);
+            ResultSet rs = pr.executeQuery();
+            if (rs.next()) {
+                rsl = new Order(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("description"),
+                        rs.getTimestamp(4)
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public boolean update(Order order, int id) {
+        try (Connection con = pool.getConnection();
+             PreparedStatement pr = con.prepareStatement("update orders set name = ?, description = ?, created = ? where id = ?",
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+            pr.setString(1, order.getName());
+            pr.setString(2, order.getDescription());
+            pr.setTimestamp(3, order.getCreated());
+            pr.setInt(4, id);
+            pr.execute();
+            ResultSet resultSetId = pr.getGeneratedKeys();
+            if (resultSetId.next()) {
+                order.setId(resultSetId.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public Order findByName(String name) {
+        Order rsl = null;
+        try (Connection con = pool.getConnection();
+             PreparedStatement pr = con.prepareStatement("select * from orders o where o.name = name")) {
+            try (ResultSet resultSet = pr.executeQuery()) {
+                while (resultSet.next()) {
+                    rsl = new Order(resultSet.getInt("id"), resultSet.getString("name"),
+                            resultSet.getString("description"),
+                            resultSet.getTimestamp("created"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+}
